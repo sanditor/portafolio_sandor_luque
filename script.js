f476479f91b161e16153082d954a4a43379c3b97
sanditor <sluque@misena.edu.co>
2023-10-29 15:49:59 -0500

    first commit1

---
+++ script.js
@@ -0,0 +1,72 @@
+//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+
+    //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
+    //en modo responsive
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+
+//función que muestra el menu responsive
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
+
+//detecto el scrolling para aplicar la animación del la barra de habilidades
+window.onscroll = function() { efectoHabilidades() };
+
+//funcion que aplica la animación de la barra de habilidades
+function efectoHabilidades() {
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 300) {
+        document.getElementById("html").classList.add("barra-progreso2");
+        document.getElementById("js").classList.add("barra-progreso3");
+        document.getElementById("bd").classList.add("barra-progreso4");
+        document.getElementById("php").classList.add("barra-progreso1");
+        document.getElementById("nodejs").classList.add("barra-progreso5");
+        document.getElementById("vuejs").classList.add("barra-progreso5");
+        document.getElementById("laravel").classList.add("barra-progreso4");
+        document.getElementById("api").classList.add("barra-progreso4");
+    }
+
+}
+
+//lógica para el lightBOx del portafolio
+const imagenes = document.querySelectorAll('.img-galeria')
+const imagenLight = document.querySelector('.agregar-imagen');
+const contenedorLight = document.querySelector('.imagen-light')
+const closeLight = document.querySelector('.close')
+
+
+imagenes.forEach(imagen => {
+    imagen.addEventListener('click',()=>{
+        aparecerImagen(imagen.getAttribute('src'));
+    })
+});
+
+contenedorLight.addEventListener('click',(e)=>{
+    if(e.target !== imagenLight){
+        contenedorLight.classList.toggle('show')
+        imagenLight.classList.toggle('showImage')
+    }
+})
+
+
+const aparecerImagen = (imagen)=>{
+    imagenLight.src = imagen;
+    contenedorLight.classList.toggle('show')
+    imagenLight.classList.toggle('showImage')
+}
