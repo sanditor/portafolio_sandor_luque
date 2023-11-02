7303009caf33de42b7e7d9c5172942371109bf6d
sanditor <sluque@misena.edu.co>
2023-11-02 17:40:55 -0500

    actualizacion4

--- script.js
+++ script.js
@@ -70,3 +70,35 @@ const aparecerImagen = (imagen)=>{
     contenedorLight.classList.toggle('show')
     imagenLight.classList.toggle('showImage')
 }
+
+//lógica para envío de formularios
+var form = document.getElementById("formContac");
+    
+async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+    }).then(response => {
+    if (response.ok) {
+        status.innerHTML = "Gracias por tu mensaje, ha sido enviado con éxito! Pronto te responderemos!";
+        form.reset()
+    } else {
+        response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+            status.innerHTML = "Oops! Hay problema al enviar tu formulario"
+        }
+        })
+    }
+    }).catch(error => {
+    status.innerHTML = "Oops! Hay problema al enviar tu formulario"
+    });
+}
+form.addEventListener("submit", handleSubmit)
